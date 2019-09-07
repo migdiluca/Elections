dac2908c857e84f414c88e0bd0ed2836747f2832
Miguel <mdiluca@itba.edu.ar>
2019-09-07 19:18:07 -0300

    InspectionService, FiscalClient

--- client/src/main/java/Elections/client/FiscalClient.java
+++ client/src/main/java/Elections/client/FiscalClient.java
@@ -1,12 +1,23 @@
 package Elections.client;
 
+import Elections.Exceptions.ElectionStateException;
+import Elections.Exceptions.ElectionsNotStartedException;
+import Elections.InspectionClient;
+import Elections.InspectionService;
 import Elections.Models.PoliticalParty;
+import com.sun.security.ntlm.Server;
 import org.kohsuke.args4j.Option;
 
 import java.io.IOException;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Optional;
 
-public class FiscalClient {
+public class FiscalClient implements InspectionClient {
 
     @Option(name = "-DserverAddress", aliases = "--server", usage = "Fully qualified ip and port where the fiscal service is located.", required = true)
     private String ip;
@@ -54,5 +65,25 @@ public class FiscalClient {
         System.out.println(client.getIp());
         System.out.println(client.getParty().name());
         System.out.println(client.getTable());
+
+        try {
+            UnicastRemoteObject.exportObject(client);
+            Registry registry = LocateRegistry.getRegistry(client.getIp(), 0);
+
+            InspectionService server = (InspectionService) registry.lookup("inspection_service");
+            server.addInspector(client, client.getParty(), client.getTable());
+
+        } catch(NotBoundException e){
+            System.out.println("Service not found, wrong name?");
+        }catch (ElectionStateException e){
+            System.out.println("The election already started or is finished");
+        } catch (RemoteException e){
+            System.out.println("Remote exception on client side on FiscalClient");
+        }
+    }
+
+    @Override
+    public void notifyVote() throws RemoteException {
+        System.out.println("New vote for " + party.name() + " on pooling place " + table.toString());
     }
 }
--- server/src/main/java/Elections/server/ServiceImpl/InspectionServiceImpl.java
+++ server/src/main/java/Elections/server/ServiceImpl/InspectionServiceImpl.java
@@ -1,22 +1,51 @@
 package Elections.server.ServiceImpl;
 
 import Elections.Exceptions.ElectionStateException;
+import Elections.InspectionClient;
 import Elections.InspectionService;
 import Elections.Models.PoliticalParty;
+import Elections.Models.Vote;
+import javafx.util.Pair;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class InspectionServiceImpl extends UnicastRemoteObject implements InspectionService {
 
     private ElectionPOJO electionState;
+
+    private Map<Pair<PoliticalParty, Integer>,List<InspectionClient>> clients;
 
     public InspectionServiceImpl(ElectionPOJO electionState) throws RemoteException {
         this.electionState = electionState;
     }
 
     @Override
-    public void addInspector(PoliticalParty party, int desk) throws RemoteException, ElectionStateException {
+    public void addInspector(InspectionClient inspectionClient, PoliticalParty party, int table) throws RemoteException, ElectionStateException {
+        Pair<PoliticalParty, Integer> votePair = new Pair<>(party,table);
+        if(clients.containsKey(votePair)){
+            clients.get(votePair).add(inspectionClient);
+        } else {
+            List<InspectionClient> clientsToNotify = new ArrayList<>();
+            clientsToNotify.add(inspectionClient);
+            clients.put(new Pair<>(party,table),clientsToNotify);
+        }
+    }
 
+    public void notifyVote(Vote vote){
+        for(PoliticalParty politicalParty : vote.getPreferredParties()){
+            List<InspectionClient> clientsToNotify = clients.get(new Pair<PoliticalParty,Integer>(politicalParty, vote.getTable()));
+            for(InspectionClient inspectionClient : clientsToNotify){
+                try{
+                    inspectionClient.notifyVote();
+                } catch (RemoteException e){
+                    System.out.println("Remote exception on server side for InspectionService");
+                }
+            }
+        }
     }
 }
