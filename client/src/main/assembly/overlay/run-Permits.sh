a7667af24bcf0603bac13ee0f4c2cf7183039834
Fermin <fgomez@itba.edu.ar>
2019-09-15 15:47:50 -0300

    Runs para los clientes

---
+++ client/src/main/assembly/overlay/run-Permits.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+chmod u+x ./run-VoteClient.sh
+chmod u+x ./run-QueryClient.sh
+chmod u+x ./run-ManagementClient.sh
+chmod u+x ./run-FiscalClient.sh
---
+++ client/src/main/assembly/overlay/run-QueryClient.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp 'lib/jars/*' "Elections.client.QueryClient" $*
