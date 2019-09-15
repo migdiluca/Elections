70cb741e197db598d20a3cdde825131fdeba2c47
Fermin <fgomez@itba.edu.ar>
2019-09-15 18:10:06 -0300

    VotingService Thread Pool

--- server/src/main/java/Elections/server/ServiceImpl/VotingServiceImpl.java
+++ server/src/main/java/Elections/server/ServiceImpl/VotingServiceImpl.java
@@ -10,24 +10,34 @@ import Elections.VotingService;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.List;
+import java.util.concurrent.*;
 
 public class VotingServiceImpl extends UnicastRemoteObject implements VotingService {
 
     private Election electionState;
+    private ExecutorService exService;
 
     public VotingServiceImpl(Election electionState) throws RemoteException {
         this.electionState = electionState;
+        exService = Executors.newFixedThreadPool(12);
     }
 
     @Override
     public void vote(List<Vote> votes) throws ElectionStateException, RemoteException {
-        if (electionState.getElectionState().equals(ElectionState.FINISHED)){
-            throw new AlreadyFinishedElectionException();
+        Future<?> future = exService.submit(() -> {
+            if (electionState.getElectionState().equals(ElectionState.FINISHED)) {
+                throw new AlreadyFinishedElectionException();
+            } else if (electionState.getElectionState().equals(ElectionState.NOT_STARTED)) {
+                throw new ElectionsNotStartedException();
+            }
+            votes.forEach(vote -> electionState.addToVoteList(vote));
+            return null;
+        });
+        try {
+            future.get();
+        } catch (InterruptedException | ExecutionException  e) {
+            throw new ElectionStateException(e.getMessage());
         }
-        else if (electionState.getElectionState().equals(ElectionState.NOT_STARTED)){
-            throw new ElectionsNotStartedException();
-        }
-        votes.forEach(vote -> electionState.addToVoteList(vote));
     }
 
 }
